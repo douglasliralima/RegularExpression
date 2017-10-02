d27590452d6608df64d40c8111c9c0cdc57dde6e
Douglas Felizardo de Lira Lima <douglasliralima@gmail.com>
2017-10-02 17:50:23 -0300

    projeto realizado para verificar uma aplicação pratica de regex

---
+++ projeto1/Padroes.java
@@ -0,0 +1,21 @@
+﻿package projeto1;
+
+public class Padroes {
+    
+//Números de CPF: cadeias de 3 dígitos, seguidos de ponto, mais 3 dígitos, ponto, 3 dígitos, hífen, 2 dígitos.
+    public boolean cpfValido(String padrao){
+        return padrao.matches("[\\d]{3}" + "." + "[\\d]{3}" + "." + "[\\d]{3}"
+        + "-" + "[\\d]{2}");
+    }
+
+//Número de telefone:Um padrão que possa reconhecer a primeira parte com 4 ou 5 dígitos(celular
+    public boolean telefoneValido(String padrao){
+        return padrao.matches("[\\d]?[\\d]{4}" + "-" + "[\\d]{4}");
+    }
+    
+//Placa de carro: 3 letrar maiusculas, hífen, 4 digitos
+    public boolean carroValido(String padrao){
+        return padrao.matches("[A-Z]{3}" + "-" + "[\\d]{4}");
+    }
+}
+
---
+++ projeto1/Projeto1.java
@@ -0,0 +1,31 @@
+﻿package projeto1;
+import java.util.*;
+
+public class Projeto1 {
+    
+//Basicamente vamos ver se a nossa classe reconhece algum dos três padrões, dado alguma sequência passada pelo usuário
+
+    public static void main(String[] args) {
+       Scanner leitor = new Scanner(System.in);
+       String entrada = "";
+       Padroes teste = new Padroes();
+       while(true){
+            entrada = leitor.nextLine();
+            if(entrada.equalsIgnoreCase("fim")) break;
+            
+            else if(teste.cpfValido(entrada)){
+                System.out.println(entrada + " é um cpf");
+            }
+
+            else if(teste.carroValido(entrada)){
+                System.out.println(entrada + " é numero de carro");
+            }
+
+            else if(teste.telefoneValido(entrada)){
+                System.out.println(entrada + " é um telefone válido");
+            }
+
+            else System.out.println("Não é valido para nenhum padrão");
+        }
+    }
+}
